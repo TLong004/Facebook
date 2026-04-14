c7bd47e99625ef4c3e8498d76dc080dcc4d471f0
TLong004 <Thanhlong2408004@gmail.com>
2026-04-14 16:32:08 +0700

    bottomnavbar

---
+++ lib/views/friend_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendListPage extends StatefulWidget {
+  const FriendListPage({super.key});
+
+  @override
+  State<FriendListPage> createState() => _FriendListPageState();
+}
+
+class _FriendListPageState extends State<FriendListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
